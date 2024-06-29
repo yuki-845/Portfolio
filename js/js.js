ff70b1aef165a552fb2d38db802e561d0a5e7bef
yicode <120433484+yuki-845@users.noreply.github.com>
2024-06-29 22:28:00 +0900

    コミット

--- js/js.js
+++ js/js.js
@@ -1,25 +1,22 @@
-// smoothTriggerにsmoothTextAppearというクラス名を付ける定義
-function SmoothTextAnime() {
-    $('.smoothTextTrigger').each(function(){ //smoothTextTriggerというクラス名が
-      var elemPos = $(this).offset().top-50;//要素より、50px上の
+function BlurTextAnimeControl() {
+    $('.smoothTextTrigger').each(function() {
+      var elemPos = $(this).offset().top - 50; // Element's position, 50px above
       var scroll = $(window).scrollTop();
       var windowHeight = $(window).height();
-      if (scroll >= elemPos - windowHeight){
-      $(this).addClass('smoothTextAppear');// 画面内に入ったらsmoothTextAppearというクラス名を追記
-      }else{
-      $(this).removeClass('smoothTextAppear');// 画面外に出たらsmoothTextAppearというクラス名を外す
+      if (scroll >= elemPos - windowHeight) {
+        $(this).addClass('combinedEffect'); // Add class when element is in view
+      } else {
+        $(this).removeClass('combinedEffect'); // Remove class when element is out of view
       }
-      }); 
+    });
   }
-  
-  
-  
-  // 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function () {
-    SmoothTextAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
-  
-  // 画面が読み込まれたらすぐに動かしたい場合の記述
-  $(window).on('load', function () {
-    SmoothTextAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+  // Trigger animation on scroll
+  $(window).scroll(function() {
+    BlurTextAnimeControl();
+  });
+
+  // Trigger animation on page load
+  $(window).on('load', function() {
+    BlurTextAnimeControl();
+  });
